9b513cf66973fdfc69f56b3dc7a49593ea03f0a5
zurira <20243ds036@utez.edu.mx>
2025-11-13 21:32:44 -0600

    Empece a realizar la vista del login, asi mismo agregue la dependencia de iconos a utilizar

--- Encuestas/src/main/java/module-info.java
+++ Encuestas/src/main/java/module-info.java
@@ -3,9 +3,10 @@ module mx.edu.utez.encuestas {
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.sql;
+    requires org.kordamp.ikonli.javafx; // Nuevo nombre del módulo para Ikonli
+    requires org.kordamp.bootstrapfx.core;
 
     requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
 
     opens mx.edu.utez.encuestas to javafx.fxml;
     exports mx.edu.utez.encuestas;
