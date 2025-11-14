383ec068d4152ec19f04b787926289a458b05068
Daniela <20243ds043@utez.edu.mx>
2025-11-14 00:51:52 -0600

    Llamada al fxml

--- Encuestas/src/main/java/mx/edu/utez/encuestas/HelloApplication.java
+++ Encuestas/src/main/java/mx/edu/utez/encuestas/HelloApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/mx/edu/utez/encuestas/views/Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Home");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
