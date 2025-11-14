6df59865cd6ba19ce7113903c375ac1b23f087e4
Daniela <20243ds043@utez.edu.mx>
2025-11-14 00:50:37 -0600

    Cambios de dependencias

--- Encuestas/src/main/java/module-info.java
+++ Encuestas/src/main/java/module-info.java
@@ -3,12 +3,12 @@ module mx.edu.utez.encuestas {
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.sql;
-
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
     opens mx.edu.utez.encuestas to javafx.fxml;
     exports mx.edu.utez.encuestas;
 
-
+    exports mx.edu.utez.encuestas.controller;
+    opens mx.edu.utez.encuestas.controller to javafx.fxml;
 }
