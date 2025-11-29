d892b19322b7a00dd1687be618d1b9ade09e7f50
TaniaSR8 <20243ds011@utez.edu.mx>
2025-11-29 13:36:56 -0600

    Modifique el HelloApplication

--- Encuestas/src/main/java/mx/edu/utez/encuestas/HelloApplication.java
+++ Encuestas/src/main/java/mx/edu/utez/encuestas/HelloApplication.java
@@ -8,9 +8,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+   /*
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/mx/edu/utez/encuestas/views/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -20,4 +21,21 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+*/
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("/mx/edu/utez/encuestas/views/hello-view.fxml")
+        );
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        stage.setTitle("Encuestas");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
