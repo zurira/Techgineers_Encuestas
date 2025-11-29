fdd6afb32e6ee8e3328ff5dd76f067f9c7ce7d0d
TaniaSR8 <202043ds011@utez.edu.mx>
2025-11-29 17:39:50 -0600

    Modifique para abrir el modal

--- Encuestas/src/main/java/mx/edu/utez/encuestas/HelloController.java
+++ Encuestas/src/main/java/mx/edu/utez/encuestas/HelloController.java
@@ -1,7 +1,14 @@
 package mx.edu.utez.encuestas;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -11,4 +18,25 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+
+
+    @FXML
+    private void abrirModalCerrarSesion() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("/mx/edu/utez/encuestas/modalcerrars.fxml")
+        );
+        Parent root = fxmlLoader.load();
+
+        Stage stage = new Stage();
+        stage.setTitle("Cerrar sesión");
+        stage.setScene(new Scene(root));
+        stage.initModality(Modality.APPLICATION_MODAL); // bloquea la ventana principal
+        stage.showAndWait();
+    }
+
+
+
+
+
 }
