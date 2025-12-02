2c731c9caf1cecf33d745dd38ded934b22668db4
zurira <20243ds036@utez.edu.mx>
2025-12-01 20:51:47 -0600

    Se inicio con la declaración de los métodos de respuesta en la interfaz

--- Encuestas/src/main/java/mx/edu/utez/encuestas/dao/IRespuesta.java
+++ Encuestas/src/main/java/mx/edu/utez/encuestas/dao/IRespuesta.java
@@ -1,4 +1,12 @@
 package mx.edu.utez.encuestas.dao;
 
+import mx.edu.utez.encuestas.model.Respuesta;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface IRespuesta {
+    boolean guardarRespuesta(Respuesta respuesta);
+    List<Respuesta> findOpcionId(int opcionId) throws SQLException;
+    List<Respuesta> findAll() throws SQLException;
 }
