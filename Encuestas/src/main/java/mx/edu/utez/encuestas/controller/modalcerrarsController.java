171bae177c38e3d4a4f7947bb47c3a8d65f8ac62
TaniaSR8 <202043ds011@utez.edu.mx>
2025-12-02 18:45:19 -0600

    Tenia conflictos para nbajar los cambios con el misc y los resolvi

--- Encuestas/src/main/java/mx/edu/utez/encuestas/controller/modalcerrarsController.java
+++ Encuestas/src/main/java/mx/edu/utez/encuestas/controller/modalcerrarsController.java
@@ -3,8 +3,14 @@ package mx.edu.utez.encuestas.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import mx.edu.utez.encuestas.HelloApplication;
+
+import java.io.IOException;
 
 public class modalcerrarsController {
 
@@ -16,12 +22,26 @@ public class modalcerrarsController {
 
     @FXML
     void onAccept(ActionEvent event) {
-        // logica para cerrar
-        System.out.println("Sesión cerrada correctamente");
+        try {
+            // Cierra el modal actual
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.close();
 
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.close();
+            // Abre la vista de login.fxml en una nueva ventana
+            FXMLLoader fxmlLoader = new FXMLLoader(
+                    HelloApplication.class.getResource("/mx/edu/utez/encuestas/login.fxml")
+            );
+            Parent root = fxmlLoader.load();
 
+            Stage loginStage = new Stage();
+            loginStage.setTitle("Login");
+            loginStage.setScene(new Scene(root));
+            loginStage.show();
 
+            System.out.println("Sesión cerrada correctamente, regresando a login...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
