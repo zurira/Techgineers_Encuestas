8fa261a0e3d71865f98eab684300ff4fd79f18ff
TaniaSR8 <20243ds011>
2025-12-03 18:46:46 -0600

    Cree esta clase para reportes

---
+++ Encuestas/src/main/java/mx/edu/utez/encuestas/controller/ReportesController.java
@@ -0,0 +1,25 @@
+package mx.edu.utez.encuestas.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import mx.edu.utez.encuestas.config.DBConnection;
+import mx.edu.utez.encuestas.dao.impl.RespuestaDaoImpl;
+import mx.edu.utez.encuestas.model.FrecuenciaDTO;
+import mx.edu.utez.encuestas.model.Usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ReportesController {
+
+
+
+
+}
