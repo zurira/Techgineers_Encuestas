28137688d2049021d31827ea312fdb533df91728
zurira <20243ds036@utez.edu.mx>
2025-12-06 13:54:11 -0600

    Se corrigio el controllador del login

--- Encuestas/src/main/java/mx/edu/utez/encuestas/controller/LoginController.java
+++ Encuestas/src/main/java/mx/edu/utez/encuestas/controller/LoginController.java
@@ -1,19 +1,19 @@
 package mx.edu.utez.encuestas.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.Region;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import mx.edu.utez.encuestas.dao.impl.UsuarioDaoImpl;
-import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import mx.edu.utez.encuestas.model.Usuario;
 import mx.edu.utez.encuestas.dao.IUsuario;
+import mx.edu.utez.encuestas.dao.impl.UsuarioDaoImpl;
+import mx.edu.utez.encuestas.model.Usuario;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.io.IOException;
@@ -31,75 +31,82 @@ public class LoginController {
     @FXML
     private void onLogin(ActionEvent event) {
         String usuario = usernameField.getText();
-        String clave = txtPassword.getText();
+        String clave = obtenerPassword();
 
         if (usuario.isEmpty() || clave.isEmpty()) {
-            mostrarAlerta("Los campos de usuario y contraseña no pueden estar vacíos");
+            mostrarAlerta("Los campos de usuario y contraseña no pueden estar vacíos.");
             return;
         }
 
-        // valida usuario en la base de datos
+        //valida credenciales
         Usuario usuarioValido = usuarioDao.validarLogin(usuario, clave);
 
-        if (usuarioValido != null) {
-            System.out.println("Inicio de sesión exitoso");
+        if (usuarioValido == null) {
+            mostrarAlerta("Usuario o contraseña incorrectos.");
+            return;
+        }
 
-            try {
-                FXMLLoader loader;
-                switch (usuarioValido.getRol().getNombre().trim().toLowerCase()) {
-                    case "administrador":
-                        System.out.println("Cargando vista admin");
-                        loader = new FXMLLoader(getClass().getResource("/mx/edu/utez/encuestas/views/dashboardAdmin.fxml"));
-                        break;
+        cargarDashboardSegunRol(usuarioValido, event);
+    }
 
-                    case "docente":
-                        System.out.println("Cargando vista de docente");
-                        loader = new FXMLLoader(getClass().getResource("/mx/edu/utez/encuestas/views/dashboardDocente.fxml"));
-                        break;
+    private String obtenerPassword() {
+        return txtPassword.isVisible()
+                ? txtPassword.getText()
+                : txtPasswordVisible.getText();
+    }
 
-                    default:
-                        mostrarAlerta("Rol no reconocido");
-                        return;
+
+    private void cargarDashboardSegunRol(Usuario usuarioValido, ActionEvent event) {
+        try {
+            FXMLLoader loader;
+
+            String rol = usuarioValido.getRol().getNombre().trim().toLowerCase();
+
+            switch (rol) {
+                case "administrador" -> loader = new FXMLLoader(getClass().getResource("/mx/edu/utez/encuestas/views/dashboardAdmin.fxml"));
+
+                case "docente" -> loader = new FXMLLoader(getClass().getResource("/mx/edu/utez/encuestas/views/dashboardDocente.fxml"));
+
+                default -> {
+                    mostrarAlerta("Rol no reconocido.");
+                    return;
                 }
-
-                Region root = loader.load();
-
-                // obtiene controlador
-                Object controller = loader.getController();
-
-                //se pasa el id de acuerdo al controllador
-                if (controller instanceof PrincipalDocenteController docenteController) {
-                    docenteController.setUsuarioActivo(usuarioValido);
-                } else if (controller instanceof PrincipalAdminController adminController) {
-                    adminController.setUsuarioActivo(usuarioValido);
-                } else if (controller instanceof DashboardDocenteController dashboardController) {
-                    dashboardController.setUsuarioActivo(usuarioValido);
-                }else if (controller instanceof DashboardAdminController dashboardController) {
-                    dashboardController.setUsuarioActivo(usuarioValido);
-                }
-
-                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                Scene scene = new Scene(root);
-                stage.setMaximized(true);
-                stage.setScene(scene);
-                stage.show();
-
-                // ajusta pantalla
-                Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-                stage.setX(screenBounds.getMinX());
-                stage.setY(screenBounds.getMinY());
-                stage.setWidth(screenBounds.getWidth());
-                stage.setHeight(screenBounds.getHeight());
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                mostrarAlerta("Error al cargar el panel.");
             }
 
-        } else {
-            mostrarAlerta("Usuario o contraseña incorrectos.");
+            Region root = loader.load();
+            Object controller = loader.getController();
+
+            // asigna usuario al controlador correspondiente
+            if (controller instanceof DashboardAdminController adminController) {
+                adminController.setUsuarioActivo(usuarioValido);
+            } else if (controller instanceof DashboardDocenteController docenteController) {
+                docenteController.setUsuarioActivo(usuarioValido);
+            }
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            stage.show();
+
+            ajustarPantalla(stage);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            mostrarAlerta("Error al cargar el panel.");
         }
     }
+
+    //ajusta la pantalla
+    private void ajustarPantalla(Stage stage) {
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setX(screenBounds.getMinX());
+        stage.setY(screenBounds.getMinY());
+        stage.setWidth(screenBounds.getWidth());
+        stage.setHeight(screenBounds.getHeight());
+    }
+
 
     private void mostrarAlerta(String mensaje) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -109,12 +116,11 @@ public class LoginController {
         alert.showAndWait();
     }
 
-
     @FXML
     private void togglePasswordVisibility(ActionEvent event) {
-        boolean showingVisible = txtPasswordVisible.isVisible();
+        boolean visible = txtPasswordVisible.isVisible();
 
-        if (showingVisible) {
+        if (visible) {
             txtPassword.setText(txtPasswordVisible.getText());
             txtPasswordVisible.setVisible(false);
             txtPasswordVisible.setManaged(false);
@@ -122,9 +128,7 @@ public class LoginController {
             txtPassword.setVisible(true);
             txtPassword.setManaged(true);
 
-            if (togglePasswordBtn.getGraphic() instanceof FontIcon icon) {
-                icon.setIconLiteral("fa-eye");
-            }
+            cambiarIcono("fa-eye");
 
         } else {
             txtPasswordVisible.setText(txtPassword.getText());
@@ -134,31 +138,36 @@ public class LoginController {
             txtPasswordVisible.setVisible(true);
             txtPasswordVisible.setManaged(true);
 
-            if (togglePasswordBtn.getGraphic() instanceof FontIcon icon) {
-                icon.setIconLiteral("fa-eye-slash");
-            }
+            cambiarIcono("fa-eye-slash");
         }
     }
 
+    private void cambiarIcono(String iconLiteral) {
+        if (togglePasswordBtn.getGraphic() instanceof FontIcon icon) {
+            icon.setIconLiteral(iconLiteral);
+        }
+    }
+
+
     @FXML
-    private void onHome(){
+    private void onHome() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/mx/edu/utez/encuestas/views/Home.fxml"));
+
             Parent root = loader.load();
 
             Stage stage = new Stage();
             stage.setTitle("Encuestas");
             stage.setScene(new Scene(root));
-            stage.show();
             stage.setMaximized(true);
+            stage.show();
 
-            //cerrar ventana
+            // Cerrar ventana actual
             Stage currentStage = (Stage) btnReturn.getScene().getWindow();
             currentStage.close();
 
         } catch (IOException e) {
-            System.err.println("Error al cargar la vista de registro: " + e.getMessage());
+            System.err.println("Error al cargar la vista Home: " + e.getMessage());
         }
-
     }
 }
